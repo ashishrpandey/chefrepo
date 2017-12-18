c70610577dfbcd3f55d5579e79484314cf4612ad
ashishrpandey <ashish.pandey@zekelabs.com>
2017-12-18 19:36:42 +0000

    Adding activemq and mysql cookbook

--- cookbooks/my2ndcookbook/recipes/my2ndrecipe.rb
+++ cookbooks/my2ndcookbook/recipes/my2ndrecipe.rb
@@ -4,9 +4,10 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-ashish['dev'] = "Pandey"
+node.default['my2ndcookbook']['dev'] = "Pandey"
 file '/etc/motd' do
-	content "This is my 2nd server with #{node['platform']} having memory of #{node['memory']['swap']['free']}" 
+#	content "This is my 2nd server with #{node['platform']} having memory of #{node['memory']['swap']['free']}" 
+	content "this is my attribute #{node['my2ndcookbook']['dev']}"
 	mode 0755
 	owner 'root'
 	group 'apache'
